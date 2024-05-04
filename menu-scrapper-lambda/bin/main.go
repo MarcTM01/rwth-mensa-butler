3d2e1b031157e1bfa4fe3b1b7fb3e3ab6eee3ed1
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2024-05-04 21:51:34 +0200

    Implemented lambda function for storing retrieved mensa information to DynamoDB in regular intervals. Updated deployment script accordingly.

--- menu-scrapper-lambda/bin/main.go
+++ menu-scrapper-lambda/bin/main.go
@@ -1,29 +1,159 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/MarcTM01/rwth-mensa-butler/menu-scrapper-lib/pkg/model"
 	"github.com/MarcTM01/rwth-mensa-butler/menu-scrapper-lib/pkg/scrapper"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"log"
+	"os"
+)
+
+var (
+	dynaClient          *dynamodb.Client
+	tableName           = os.Getenv("TABLE_NAME")
+	environmentName     = os.Getenv("ENVIRONMENT_NAME")
+	scrapConfigurations *model.ApplicationConfiguration
 )
 
 type Response struct {
-	Success bool   `json:"success"`
-	Message string `json:"message"`
+	Success  bool     `json:"success"`
+	Messages []string `json:"message"`
+}
+
+func (r *Response) Merge(other *Response) *Response {
+	r.Success = r.Success && other.Success
+	r.Messages = append(r.Messages, other.Messages...)
+	return r
+}
+
+type MensaOfferingsTable struct {
+	MensaIdLanguageKeyDate string
+	MensaId                string
+	LanguageKey            string
+	model.MensaDayMenus
+}
+
+func storeDayOfferings(mensaId string, languageKey string, offerings model.MensaDayMenus) error {
+	tableEntry := MensaOfferingsTable{
+		MensaIdLanguageKeyDate: mensaId + ";" + languageKey + ";" + offerings.Date,
+		MensaId:                mensaId,
+		LanguageKey:            languageKey,
+		MensaDayMenus:          offerings,
+	}
+
+	item, err := attributevalue.MarshalMap(tableEntry)
+	if err != nil {
+		return err
+	}
+	_, err = dynaClient.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: aws.String(tableName), Item: item,
+	})
+	return err
+}
+
+func retrieveAndStoreOfferingsForMensaUrl(mensaId string, languageKey string, url string) Response {
+	response := Response{
+		Success:  true,
+		Messages: []string{},
+	}
+
+	offerings, err := scrapper.RetrieveCurrentMensaOfferings(url)
+	if err != nil {
+		response.Success = false
+		response.Messages = append(response.Messages, fmt.Sprintf("[%s-%s] Could not scrap offerings from website", mensaId, languageKey))
+		log.Printf("[%s-%s] Could not scrap offerings from website: %v", mensaId, languageKey, err)
+		return response
+	}
+
+	successCounter := 0
+	for _, dailyOfferings := range offerings.DailyMenus {
+		err = storeDayOfferings(mensaId, languageKey, dailyOfferings)
+		if err != nil {
+			response.Success = false
+			response.Messages = append(response.Messages, fmt.Sprintf("[%s-%s] Could not store scrapped offerings to database for date %s", mensaId, languageKey, dailyOfferings.Date))
+			log.Printf("[%s-%s] Could not store scrapped offerings to database for date %s: %v", mensaId, languageKey, dailyOfferings.Date, err)
+		} else {
+			successCounter++
+		}
+	}
+
+	response.Messages = append(response.Messages, fmt.Sprintf("[%s-%s] Successfully stored %d days of offerings", mensaId, languageKey, successCounter))
+	log.Printf("[%s-%s] Successfully stored %d days of offerings", mensaId, languageKey, successCounter)
+	return response
+}
+
+func retrieveAndStoreOfferingsForMensa(mensa model.MensaConfiguration) Response {
+	response := Response{
+		Success:  true,
+		Messages: []string{},
+	}
+
+	responseDe := retrieveAndStoreOfferingsForMensaUrl(mensa.MensaId, "de", mensa.Urls.GermanUrl)
+	response.Merge(&responseDe)
+
+	responseEn := retrieveAndStoreOfferingsForMensaUrl(mensa.MensaId, "en", mensa.Urls.EnglishUrl)
+	response.Merge(&responseEn)
+
+	return response
 }
 
 func handler() (*Response, error) {
-	configuration, err := scrapper.GetApplicationConfiguration()
-	if err != nil {
-		return nil, err
+	response := Response{
+		Success:  true,
+		Messages: []string{},
 	}
 
-	results, err := scrapper.RetrieveCurrentMensaOfferings(configuration.MensaConfigurations[0].Urls.GermanUrl)
+	for _, mensa := range scrapConfigurations.MensaConfigurations {
+		mensaResponse := retrieveAndStoreOfferingsForMensa(mensa)
+		response.Merge(&mensaResponse)
+	}
 
-	return &Response{
-		Success: true,
-		Message: results.MensaName,
-	}, nil
+	return &response, nil
+}
+
+func getAwsConfiguration() (aws.Config, error) {
+	if environmentName == "localdev" {
+		return config.LoadDefaultConfig(context.TODO(),
+			config.WithRegion("localhost"),
+			config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(
+				func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+					return aws.Endpoint{URL: "http://host.docker.internal:8000"}, nil
+				})),
+			config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
+				Value: aws.Credentials{
+					AccessKeyID: "something", SecretAccessKey: "somethingsecret", SessionToken: "",
+					Source: "This stuff is just random.",
+				},
+			}))
+	} else if environmentName == "prod" {
+		return config.LoadDefaultConfig(context.TODO())
+	} else {
+		return aws.Config{}, fmt.Errorf("unknown environment name: %s", environmentName)
+	}
 }
 
 func main() {
+	// Load AWS configuration
+	cfg, err := getAwsConfiguration()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create DynamoDB client
+	dynaClient = dynamodb.NewFromConfig(cfg)
+
+	// Load scrapping configuration
+	scrapConfigurations, err = scrapper.GetApplicationConfiguration()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	lambda.Start(handler)
 }
--- menu-scrapper-lambda/go.mod
+++ menu-scrapper-lambda/go.mod
@@ -10,8 +10,26 @@ require (
 require (
 	github.com/PuerkitoBio/goquery v1.9.1 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.26.1 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.27.11 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.11 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.13.14 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.32.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.20.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.9.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.20.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.28.6 // indirect
+	github.com/aws/smithy-go v1.20.2 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
