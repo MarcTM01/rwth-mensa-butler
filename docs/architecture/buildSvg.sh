6550aab3dd6a135a9434beee034cb5758be73597
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2024-07-23 17:50:47 +0200

    Pin version of drawio-desktop in bash script to prevent flaky CI pipeline

--- docs/architecture/buildSvg.sh
+++ docs/architecture/buildSvg.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # Step 1: Build the SVG file from the drawio file
-docker run --rm -v "$(pwd)":/pwd rlespinasse/drawio-desktop-headless -x --embed-svg-images -t -f svg -o /pwd/architecture-orig.svg /pwd/architecture.drawio
+docker run --rm -v "$(pwd)":/pwd rlespinasse/drawio-desktop-headless:v1.35.0 -x --embed-svg-images -t -f svg -o /pwd/architecture-orig.svg /pwd/architecture.drawio
 
 # Step 2: Inject the darkmode CSS into the SVG file
 awk 'BEGIN{getline l < "darkmode-style-injection.txt"}/<defs\/>/{gsub("<defs/>",l)}1' architecture-orig.svg > architecture.svg
